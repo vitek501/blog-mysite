7dcb68b0e03fb268e72bc3182b58edcfbb67ad5b
Viktor Kurdyukov <vitek.kurdyukov@gmail.com>
2021-04-02 10:16:25 +0500

    add psql and search

--- mysite/settings.py
+++ mysite/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'taggit',
     'django.contrib.sites',
     'django.contrib.sitemaps',
+    'django.contrib.postgres',
 ]
 
 MIDDLEWARE = [
@@ -82,8 +83,10 @@ EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'blogdb',
+        'USER': 'blog',
+        'PASSWORD': '****',
     }
 }
 
